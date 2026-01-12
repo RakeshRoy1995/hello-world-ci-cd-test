5eb42ccd160cebb3dcd807a7a0acd56356a341a9
Rakesh Roy <rakeshroyshuvo@gmail.com>
2026-01-12 17:42:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello World from Node.js 🚀");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "🚀 Express app is running on EC2!",
+    time: new Date().toISOString(),
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
